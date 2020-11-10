1c6e5022f351af7066795cfb202699540bab45be
NavPoint1 <foleym156@gmail.com>
2020-11-10 07:38:25 -0800

    status effect battle functional

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,11 +1,11 @@
 class TrainersController < ApplicationController
     def index
         trainers = Trainer.all
-        render json: trainers.to_json(include: {trainer_category: {}, pokemons: {include: [:species => {include: [:types]}, :status_effect => {}, :moves => {include: [:move_status_effects => {include: [:status_effect]}, :status_effects, :type]}]}})
+        render json: trainers.to_json(include: {trainer_category: {}, pokemons: {include: [:species => {include: [:types]}, :status_effect => {}, :moves => {include: [:move_status_effects => {include: [:status_effect]}, :status_effects => {}, :type => {}]}]}})
     end
     
     def show
         trainer = Trainer.find(params[:id])
-        render json: trainer.to_json(include: [:trainer_category, :pokemons => {include: [:species => {include: [:types]}, :status_effect => {}, :moves => {include: [:move_status_effects => {include: [:status_effect]}, :status_effects, :type]}]}])
+        render json: trainer.to_json(include: [:trainer_category, :pokemons => {include: [:species => {include: [:types]}, :status_effect => {}, :moves => {include: [:move_status_effects => {include: [:status_effect]}, :status_effects => {}, :type => {}]}]}])
     end
 end
